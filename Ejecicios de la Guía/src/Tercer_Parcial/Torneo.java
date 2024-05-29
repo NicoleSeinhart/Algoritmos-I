f9e03a88c542c688ff14a09bcbe0d2d39acd6d70
NicoleSeinhart <nicole.seinhart@gmail.com>
2024-05-29 18:22:41 -0300

    Nuevo

--- Ejecicios de la Guía/src/Tercer_Parcial/Torneo.java
+++ Ejecicios de la Guía/src/Tercer_Parcial/Torneo.java
@@ -3,7 +3,6 @@ package Tercer_Parcial;
 import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.time.LocalDate;
 
